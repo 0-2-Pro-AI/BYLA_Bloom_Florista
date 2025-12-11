798b213563c50b04a048ee4c8348840aee7b886c
GOAT <tongng219@gmail.com>
2025-12-11 17:50:57 +0000

    .

--- setup.py
+++ setup.py
@@ -1,46 +1,72 @@
 import pandas as pd 
+import os
 
-FILE_USERS = "users.csv"
-FILE_PRODUCTS = "products.csv"
+FILE_CLIENTS = "login_client.csv"
+FILE_STAFF = "works_delivery.csv"
+FILE_PRODUCTS = "products_stock.csv"
 FILE_ORDERS = "orders.csv"
 FILE_COMPLAINTS = "complaints.csv"
 
-def create_data():
-    print("Being processing the prototype....")
+def create_data_final():
+    print("Checking data system....")
 
-    users_data = {
-        "id": ["admin", "client1", "ship1"],
-        "pass": ["123","123","123"],
-        "role": ["manager","client","estafeta"],
-        "name": ["Manager A", "Client B", "Shipper C"]
-    }
+    if not os.path.exists(FILE_CLIENTS):
+        data_clients = {
+            "Name": ["Joao Pereira", "Maria Fernandes"],
+            "Contact": ["961219231", "930153233"],
+            "Password": ["client1", "client2"],
+            "Address": ["Travessia Amelia Rodrigues", "Rua Antonio Fonseca"],
+            "ZP1": ["152", "743"],
+            "ZP2": ["4700", "4720"],
+            "Orders": ["103", "336"]
+        }
 
-    df_users = pd.DataFrame(users_data)
-    df_users.to_csv(FILE_USERS, index=False)
-    print(f"{FILE_USERS} has been created!")
+        pd.DataFrame(data_clients).to_csv(FILE_CLIENTS, index=False, sep=";")
+        print(f"Newly created: {FILE_CLIENTS}")
+    else: 
+        print(f"Already exist: {FILE_CLIENTS} (keep)")
 
-    products_data = {
-        "id": [101,102,103],
-        "name": ["Red Flower", "Blue Flower", "Pink Flower"],
-        "price": [4,5,6],
-        "stock": [10,20,50]
-    }
-    df_products = pd.DataFrame(products_data)
-    df_products.to_csv(FILE_PRODUCTS, index=False)
-    print(f"{FILE_PRODUCTS} has been created!")
-    
-    columns_orders = ["order_id", "client_id","product_id","status","shipper_id"]
-    df_orders = pd.DataFrame(columns=columns_orders)
-    df_orders.to_csv(FILE_ORDERS, index=False)
-    print(f"{FILE_ORDERS} has been created!")
+    if not os.path.exists(FILE_STAFF):
+        data_staff = {
+            "Login": ["109609", "113168"],
+            "Password": ["Yeidman", "Andre"],
+            "Zone": ["Gestor", "Center"],
+            "Work_Hour": ["all_day", "week_morning"]
+        }
+        pd.DataFrame(data_staff).to_csv(FILE_STAFF, index=False, sep=';')
+        print(f"Newly created: {FILE_STAFF}")
+    else:
+        print(f"Already exist: {FILE_STAFF} (keep)")
 
-    columns_complaints = ["order_id","comment"]
-    df_complaints = pd.DataFrame(columns=columns_complaints)
-    df_complaints.to_csv(FILE_COMPLAINTS, index=False)
-    print(f"{FILE_COMPLAINTS} has been created!")
+    if not os.path.exists(FILE_PRODUCTS):
+        data_products = {
+            "Id": [2001, 2002, 2003, 2004, 2005],
+            "Product": ["Flores variadas", "Rosas", "Lirios", "Tuplipas", "Margaridas"],
+            "Quantity in stock": [250, 500, 150, 175, 200],
+            "Unite price": ["3,00", "5,00", "12,00", "15,00", "8,00"]
+        }
+        pd.DataFrame(data_products).to_csv(FILE_PRODUCTS, index=False, sep=';')
+        print(f"Newly created: {FILE_PRODUCTS}")
+    else:
+        print(f"Already exist: {FILE_PRODUCTS} (keep)")
 
-    print("DONE! The storage is ready!")
+    if not os.path.exists(FILE_ORDERS):
+        cols = ["order_id", "client_id", "product_id", "status", "shipper_id"]
+        pd.DataFrame(columns=cols).to_csv(FILE_ORDERS, index=False, sep=';')
+        print(f"Newly created: {FILE_ORDERS}")
+    else:
+        print(f"Already exist: {FILE_ORDERS} (keep)")
+
+
+    if not os.path.exists(FILE_COMPLAINTS):
+        cols =["order_id", "content"]
+        pd.DataFrame(columns=cols).to_csv(FILE_COMPLAINTS, index=False, sep=';')
+        print(f"Newly created: {FILE_COMPLAINTS}")
+    else:
+        print(f"Already exist: {FILE_COMPLAINTS}")
+
+    print("Done!")
 
 if __name__ == "__main__":
-    create_data()
+    create_data_final()
 
